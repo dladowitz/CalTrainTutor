3cbf402b5f191656a7ec5502992a19f67e821e08
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-07-28 10:02:21 -0700

    jquery mobile, lessons

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ CaltrainTutor::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'lessons#index'
 
   # See how all your routes lay out with "rake routes"
 
