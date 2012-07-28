fb8af035be4f19f6a6c3c4e202fa187001c58e07
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2012-07-28 00:11:29 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaltrainTutor::Application.config.secret_token = 'a70fea8ffcfed03252ac1b5ddc807d7632677d015718baefd948b56ab37c7e6854afa04d53d3a7088d2ce163b355873f01d84bd6e9419a292553dd4081c2f020'
