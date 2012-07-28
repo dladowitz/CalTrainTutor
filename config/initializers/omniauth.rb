9e34d0a971a99dc7397bc1dfd44a430f7f67636e
David Ladowitz <david@ladowitz.com>
2012-07-28 16:33:14 -0700

    Added birthday to User model and view

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, '399084033489037', 'df441fe6a567e3e607b8c6570864af51'
+    provider :facebook, '399084033489037', 'df441fe6a567e3e607b8c6570864af51',
+             :scope => 'user_birthday'
   else
-    provider :facebook, '226464100760780', 'bb12366f7ecccd3bbb9a8119acdd47bf'
+    provider :facebook, '226464100760780', 'bb12366f7ecccd3bbb9a8119acdd47bf',
+             :scope => 'user_birthday'
   end
 end
